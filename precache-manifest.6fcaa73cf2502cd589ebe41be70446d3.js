64bb5cb2183d7eb61197be525581e06b05b5c80d
Raj Gaurav Gupta <149098451+rajGauravGupta2025@users.noreply.github.com>
2025-05-09 14:49:22 +0530

    Add files via upload

---
+++ precache-manifest.6fcaa73cf2502cd589ebe41be70446d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ecc503c51db0f37e83d56ecdbbe622f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e46e5560fcf9eee6082",
+    "url": "/static/css/2.7f1403ef.chunk.css"
+  },
+  {
+    "revision": "a3a8c1302d375414e888",
+    "url": "/static/css/main.4289a7c4.chunk.css"
+  },
+  {
+    "revision": "8e46e5560fcf9eee6082",
+    "url": "/static/js/2.335d95f6.chunk.js"
+  },
+  {
+    "revision": "a3a8c1302d375414e888",
+    "url": "/static/js/main.f5eeb2bf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/static/media/logo.9f7e506d.svg"
+  }
+]);
